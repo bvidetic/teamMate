b65cfcd2f3943b53f1b385eb441d2786a15a369e
brian <videticb@gmail.com>
2014-06-27 17:23:35 -0700

    javascript update

--- html/main.js
+++ html/main.js
@@ -1,11 +1,57 @@
 $(document).ready(function(){
+    function hideBoxes(){
+        $(".centerMiddle, .register, .signinin, .login, .forgotPassword") .hide();
+    };
 
-    $('body').on('click', '#findPlayers', function() {
-        $("#hideRegister").fadeIn(1000);
-        $(".centerMiddle").fadeout(1000);
-        });
+    $("#findPlayers1, #findPlayers2, #signinin, #lostPassword").on("click", function(){
 
+        var $this = $(this);
 
+        if ($this.html() == "Search for Players"){
+            hideBoxes();
+            $(".register").show();
+        } else { 
+            $(".mainContents").show();
+            $("#signinin").show();
+        }
 
+        if ($this.html() == "Search for Teams"){
+            hideBoxes();
+            $(".register").show();
+        } else {
+            $(".mainContents").show();
+            $("#signinin").show();
+        }
 
+        if ($this.html() == "Sign In"){
+            hideBoxes();
+            $(".login").show();
+        } else {
+            $(".mainContents").show();
+            $("#signinin").show();
+        }
+
+        if ($this.html() == "Forgot Password"){
+            hideBoxes();
+            $(".forgotPassword").show();
+        } else {
+            $(".mainContents").show();
+            $("#signinin").show();
+        }
+    });
+    
+
+    $("#useSelButton").on("click", function(){
+        if (document.userSelectionForm.role[0].checked){
+           $(".teamManager").show();
+           $(".playerProfile").hide();
+        }
+
+        if (document.userSelectionForm.role[1].checked){
+           $(".playerProfile").show();
+           $(".teamManager").hide();
+        }
+        return false;
+    });
+   
 });
